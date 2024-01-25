f0b829ba2a348b0b266a303a61958fffa7e6ba99
Masya <Masha-Sveta.1@yandex.ru>
2024-01-25 22:06:15 +0300

    задание 26 удаление повторюшек из массива

---
+++ 26_remove_duplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        int k = 0;
+        
+        int len = nums.size();
+        for(int i = 1; i < len; i++)
+        {
+            if(nums[i] == nums[k])
+                continue;
+            else
+            {
+                ++k;
+                nums[k] = nums[i];
+            }           
+        }
+        return k+1;
+    }
+};
