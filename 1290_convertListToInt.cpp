4e3e01e51c4ed2ce8b477cdaae49d72b67284ce8
Masya <Masha-sveta.1@yandex.ru>
2024-01-28 17:25:29 +0300

    1ya zadacha na spiski

---
+++ 1290_convertListToInt.cpp
@@ -0,0 +1,27 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     int getDecimalValue(ListNode *head)
+//     {
+//         int res = 0;
+
+//         ListNode *cur = head;
+//         while (cur != nullptr)
+//         {
+//             res *= 2;
+//             res += cur->val;
+//             cur = cur->next;
+//         }
+//         return res;
+//     }
+// };
