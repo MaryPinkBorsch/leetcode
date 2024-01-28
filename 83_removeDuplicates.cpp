6dc7e4f0e01e87d4a60ae9b2e767922e1252e6a5
Masya <Masha-sveta.1@yandex.ru>
2024-01-28 17:39:00 +0300

    2ya zadacha na spiski

---
+++ 83_removeDuplicates.cpp
@@ -0,0 +1,39 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     ListNode *deleteDuplicates(ListNode *head)
+//     {
+//         ListNode *cur = head;
+//         ListNode *prev = nullptr;
+//         while (cur != nullptr)
+//         {
+//             if (prev)
+//             {
+//                 if (prev->val == cur->val)
+//                 {
+//                     prev->next = cur->next;
+//                 }
+//                 else
+//                 {
+//                     prev = cur;
+//                 }
+//             }
+//             else
+//             {
+//                 prev = cur;
+//             }
+//             cur = cur->next;
+//         }
+//         return head;
+//     }
+// };
