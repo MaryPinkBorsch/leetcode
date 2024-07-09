54445df8a7413da1ab4ce27807a824a7f3570bcc
Masya <Masha-Sveta.1@yandex.ru>
2024-07-09 23:04:32 +0300

    da

--- 2545_sort_score_matriza.cpp
+++ 2545_sort_score_matriza.cpp
@@ -32,3 +32,15 @@
 // };
 
 // https://leetcode.com/problems/sort-the-students-by-their-kth-score/
+
+
+//bonus
+
+// class Solution1 {
+// public:
+//     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) 
+//     {
+//         std::sort( score.begin(), score.end(), [&](const vector<int> & lhs, const vector<int> & rhs) {  return lhs[k] > rhs[k]; } );
+//         return score;
+//     }
+// };
